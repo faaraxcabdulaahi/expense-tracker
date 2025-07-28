ee1dff35065988898a86a4e5d2326b03e1364c56
faaraxcabdulaahi <barashoonline2021@gmail.com>
2025-07-28 07:15:15 +0300

    feat: implement user schema with name, email, password, role  validation and timestamps

--- backend/model/userModel.ts
+++ backend/model/userModel.ts
@@ -8,3 +8,12 @@ export interface IUSER extends Document {
   role: "user" | "admin";
   comparePassword(enteredPassword: string): Promise<boolean>;
 }
+
+const userSchema = new Schema<IUSER>({
+    name:{type:String, required:true},
+    email:{type:String, required:true, unique:true, lowercase:true},
+    password:{type:String, required:true},
+    role:{type:String, enum:["user", "admin"], default:"user"}
+},{
+    timestamps:true
+});
