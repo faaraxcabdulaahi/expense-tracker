05e6304e44299e6e9619e3ad5f0defec952ce775
faaraxcabdulaahi <barashoonline2021@gmail.com>
2025-07-29 15:12:36 +0300

    feat: add zod validation for loginSchema

--- backend/validations/authValidation.ts
+++ backend/validations/authValidation.ts
@@ -6,3 +6,7 @@ export const registerSchema = z.object({
   password: z.string().min(6, "Password must be at least 6 chars"),
 });
 
+export const loginSchema = z.object({
+  email: z.string().email("Invalid email"),
+  password: z.string().min(1, "Password is required"),
+});
