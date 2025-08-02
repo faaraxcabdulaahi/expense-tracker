0ba477cecab337359dac33af611b8d853653d555
faaraxcabdulaahi <barashoonline2021@gmail.com>
2025-08-02 14:32:25 +0300

    add admin middleware and profile route for authenticated users

--- backend/src/routes/authRoute.ts
+++ backend/src/routes/authRoute.ts
@@ -1,10 +1,15 @@
 import express from "express";
 import { login, register } from "../controllers/authController";
+import { protect } from "../middleware/authMiddlware";
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
+router.get("/profile", protect, (req, res) => {
+  res.status(200).json(req.user);
+});
+
 
 
 export default router;
