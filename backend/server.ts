594286381e127d970f68a6689a0e316b4ca06262
faaraxcabdulaahi <barashoonline2021@gmail.com>
2025-08-02 10:40:39 +0300

    fixed the .js extension causing errors which was occuring in dist folder which node js is using it for production

--- backend/server.ts
+++ backend/server.ts
@@ -1,9 +1,12 @@
 import express, {Express} from "express";
 import connectToDb from "./config/db.js";
-
+import authRoutes from "./routes/authRoute.js"
 
 const app:Express = express();
 
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
 
 
 connectToDb(app);
