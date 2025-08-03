efdd50d36cc831c0a721c06991d66ea0f313191e
faaraxcabdulaahi <barashoonline2021@gmail.com>
2025-08-03 16:15:23 +0300

    Add protected transaction routes with CRUD and monthly summary endpoints

---
+++ backend/src/routes/transactionRoute.ts
@@ -0,0 +1,15 @@
+import { createTransaction, deleteTransaction, getMonthlySummary, getTransactions, updateTransaction } from "@/controllers/transactionController";
+import { protect } from "@/middleware/authMiddlware";
+import express from "express";
+
+const router = express.Router();
+
+router.use(protect);
+
+router.post("/", createTransaction);
+router.get("/", getTransactions);
+router.get("/monthly-summary", getMonthlySummary);
+router.put("/:id", updateTransaction);
+router.delete("/:id", deleteTransaction);
+
+export default router;
